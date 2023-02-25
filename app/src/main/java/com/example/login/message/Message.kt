fc41e0eacd7cde3ebcc075f530e16e3eed63f34d
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-02-25 10:21:56 +0900

    간단한 애러 고침, 채팅방에 들어오는 코드 & 메시지를 보낼수 있는 코드 분리

--- app/src/main/java/com/example/login/message/Message.kt
+++ app/src/main/java/com/example/login/message/Message.kt
@@ -1,6 +1,9 @@
 package com.example.login.message
 
+import java.util.Date
+
 data class Message(
     val message: String,
-    val sender: Boolean
+    val sender: Boolean,
+//    val date: Date TODO 나중에는 메시지 시간도 할예정
 )
--- app/src/main/java/com/example/login/presentation/Fragment/ChatFragment.kt
+++ app/src/main/java/com/example/login/presentation/Fragment/ChatFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.login.adapter.MessageAdapter
@@ -55,13 +56,18 @@ class ChatFragment : Fragment() {
         val view = binding.root
         val roomName = arguments?.getString("roomName")
         var roomId = arguments?.getString("roomId")
-
-
+        stompClient.connect() //소켓 연결
+        val data = JSONObject()
+        data.put("type","ENTER")
+        data.put("roomId",roomId)
+        data.put("sender","HamTory")
+        data.put("message","")
+        stompClient.send("/pub/chat/send",data.toString()).subscribe()
 
         binding.messageButton.setOnClickListener {
 
             mData.add(Message(binding.messageText.text.toString(),true))
-            runStomp(roomId!!,binding.messageText.text.toString())
+            SocketStompMessage(roomId!!,binding.messageText.text.toString())
             binding.messageText.text = null
             binding.recyclerview.adapter = mAdapter
             mAdapter.notifyDataSetChanged()
@@ -73,23 +79,30 @@ class ChatFragment : Fragment() {
         return view
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        stompClient.disconnect() //소켓 연결 해제
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         mbinding = null
     }
 
-    fun runStomp(roomId: String, text: String){
-        stompClient.topic("/sub/chat/user/cksgur0612@dgsw.hs.kr34ce07dd-7c66-4d5c-ae77-2544fb35c875").subscribe{
-            mData.add(Message(binding.messageText.text.toString(),false))
+    private fun SocketStompMessage(roomId: String, text: String){
+        stompClient.topic("/sub/chat/user/cksgur0612@dgsw.hs.kr"+"$roomId").subscribe{//message 받는거 34ce07dd-7c66-4d5c-ae77-2544fb35c875
+            Log.d("상태","$it")
+            mData.add(Message(it.toString(),false)) //TODO it에서 오는 값중 message만 따로 빼서 리사이클러뷰에 띄워야함
             binding.recyclerview.adapter = mAdapter
             mAdapter.notifyDataSetChanged()
         }
-        val headerList = arrayListOf<StompHeader>()
-        headerList.add(StompHeader("type","ENTER"))
-        headerList.add(StompHeader("roomId",roomId))
-        headerList.add(StompHeader("sender","HamTory"))
-        headerList.add(StompHeader("message",text))
-        stompClient.connect(headerList)
+
+//        val headerList = arrayListOf<StompHeader>()
+//        headerList.add(StompHeader("type","ENTER"))
+//        headerList.add(StompHeader("roomId",roomId))
+//        headerList.add(StompHeader("sender","HamTory"))
+//        headerList.add(StompHeader("message",text))
+
 
         stompClient.lifecycle().subscribe(){
             when(it.type){
@@ -102,6 +115,7 @@ class ChatFragment : Fragment() {
                 LifecycleEvent.Type.ERROR -> {
                     Log.d("상태","ERROR")
                     Log.e("상태",it.exception.toString())
+                    Toast.makeText(context, "error: ${it.exception}", Toast.LENGTH_SHORT).show()
                 }
                 else -> {
                     Log.d("상태",it.message)
@@ -109,10 +123,12 @@ class ChatFragment : Fragment() {
             }
         }
         val data = JSONObject()
-        data.put("type","ENTER")
+        data.put("type","TALK")
         data.put("roomId",roomId)
         data.put("sender","HamTory")
         data.put("message",text)
-        stompClient.send("/pub/chat/send",data.toString()).subscribe()
+        stompClient.send("/pub/chat/send",data.toString()).subscribe() //메시지 보내는것
     }
+
+
 }

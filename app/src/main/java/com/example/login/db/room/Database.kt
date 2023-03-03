e2cca93e8e914c7acc2b4ea1972392b7757daac9
HamTory_06 <ghkdwndhks@dgsw.hs.kr>
2023-03-03 21:03:52 +0900

    Realm -> Room 메시지를 저장하는데 Room이 더 편한거같음

--- app/src/main/java/com/example/login/db/room/Database.kt
+++ app/src/main/java/com/example/login/db/room/Database.kt
@@ -2,8 +2,11 @@ package com.example.login.db.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.login.db.room.dao.MessageDao
+import com.example.login.db.room.dao.RoomDao
 
 @Database(entities = [RoomMessage::class], version = 1)
 abstract class Database: RoomDatabase(){
-    abstract fun messageDao(): RoomMessageDao
+    abstract fun messageDao(): MessageDao
+    abstract fun roomDao(): RoomDao
 }
--- app/src/main/java/com/example/login/db/room/RoomMessageDao.kt
+++
@@ -1,8 +0,0 @@
-package com.example.login.db.room
-
-import androidx.room.Dao
-
-@Dao
-interface RoomMessageDao {
-
-}
